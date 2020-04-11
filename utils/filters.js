56258434fbc4f9f636b2fc91693fbd45d0bd1ff5
Max Böck <hello@mxb.at>
2020-04-11 23:21:48 +0200

    add 404

--- utils/filters.js
+++ utils/filters.js
@@ -12,5 +12,13 @@ module.exports = {
             includeOffset: false,
             suppressMilliseconds: true
         })
+    },
+
+    obfuscate: function (str) {
+        const chars = []
+        for (var i = str.length - 1; i >= 0; i--) {
+            chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
+        }
+        return chars.join('')
     }
 }
