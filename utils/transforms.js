c58fb43d308f2902704bc782394afb14b73ec020
Max Böck <hello@mxb.at>
2020-04-12 20:39:05 +0200

    add critical css transform

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,17 +1,41 @@
 const htmlmin = require('html-minifier')
+const critical = require('critical')
+process.setMaxListeners(Infinity)
+
+const buildDir = 'dist'
+
+const shouldTransformHTML = (outputPath) =>
+    outputPath &&
+    outputPath.endsWith('.html') &&
+    process.env.ELEVENTY_ENV === 'production'
+
+const isHomePage = (outputPath) => outputPath === `${buildDir}/index.html`
 
 module.exports = {
     htmlmin: function (content, outputPath) {
-        if (
-            outputPath &&
-            outputPath.endsWith('.html') &&
-            process.env.ELEVENTY_ENV === 'production'
-        ) {
+        if (shouldTransformHTML(outputPath)) {
             return htmlmin.minify(content, {
                 useShortDoctype: true,
                 removeComments: true,
                 collapseWhitespace: true
             })
+        }
+        return content
+    },
+
+    critical: async function (content, outputPath) {
+        if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
+            const config = {
+                base: `${buildDir}/`,
+                html: content,
+                inline: true,
+                minify: true,
+                width: 1280,
+                height: 800,
+                timeout: 30000
+            }
+            const html = await critical.generate(config)
+            return html
         }
         return content
     }
