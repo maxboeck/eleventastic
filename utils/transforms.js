8976a8e002514ab103e4a7f541196519e28018c6
Max Böck <hello@mxb.at>
2020-08-22 12:17:42 +0200

    fix critical config for v2.0.3, HT @leifniem

--- utils/transforms.js
+++ utils/transforms.js
@@ -30,10 +30,9 @@ module.exports = {
                     html: content,
                     inline: true,
                     width: 1280,
-                    height: 800,
-                    timeout: 30000
+                    height: 800
                 }
-                const html = await critical.generate(config)
+                const { html } = await critical.generate(config)
                 return html
             } catch (err) {
                 console.error(err)
